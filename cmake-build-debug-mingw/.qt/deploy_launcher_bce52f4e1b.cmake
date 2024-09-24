aa14eed0a4b1045a0fb4a2b1c8fb7ed45f79040a
Martin Prejsa <martinprejsa@duck.com>
2024-09-24 14:41:23 +0200

    remove build dir

--- cmake-build-debug-mingw/.qt/deploy_launcher_bce52f4e1b.cmake
+++
@@ -1,9 +0,0 @@
-include(C:/Users/Martin/cxx/launcher/cmake-build-debug-mingw/.qt/QtDeploySupport.cmake)
-include("${CMAKE_CURRENT_LIST_DIR}/launcher-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_ALL_MODULES_FOUND_VIA_FIND_PACKAGE "ZlibPrivate;EntryPointPrivate;Core;Gui;Widgets")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE C:/Users/Martin/cxx/launcher/cmake-build-debug-mingw/launcher.exe
-    GENERATE_QT_CONF
-    NO_TRANSLATIONS
-)
